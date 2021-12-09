7a3c07a0ccfb5110440d57cd280c2fb8f3bdcb17
Jesus Conde <educaleft@gmail.com>
2021-12-09 00:48:07 +0000

    Añadido ejemplo de variables globales constantes, con errores por definir dos variables const sin inicializarlas

--- VariablesGlobales/VariablesGlobales.cpp
+++ VariablesGlobales/VariablesGlobales.cpp
@@ -1,5 +1,53 @@
 #include <iostream>
 
+const int g_x; // error: variables constantes deben ser inicializadas
+constexpr int g_w; // error: variables constexpr deben ser inicializadas
+
+const int g_y{ 1 };  // variable global const g_y, inicializada con un valor
+constexpr int g_z{ 2 }; // variable global constexpr g_z, inicializada con un valor
+
+void hacerAlgo()
+{
+    // variables globales pueden ser vistas y usadas en cualquier lugar del archivo
+    std::cout << g_y << '\n';
+    std::cout << g_z << '\n';
+}
+
+int main()
+{
+    hacerAlgo();
+
+    // variables globales pueden ser vistas y usadas en cualquier lugar del archivo
+    std::cout << g_y << '\n';
+    std::cout << g_z << '\n';
+
+    return 0;
+}
+// g_y y g_z  salen de alcance aquí
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+
 // inicializaciones variables globales
 int g_x; // sin inicializados explícito ( inicializada a cero de modo predeterminado)
 int g_y{}; // inicializada a cero
@@ -26,4 +74,4 @@ int main()
 
     return 0;
 }
-// v_g sale de alcance aquí
+// v_g sale de alcance aquí*/
